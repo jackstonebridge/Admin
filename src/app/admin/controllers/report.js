85c459e18756bb998b460fef1daa1de215cdacca
Martin Zeman <martin.zeman@protonmail.com>
2017-01-12 05:05:01 +0100

    Bugfixes

--- src/app/admin/controllers/report.js
+++ src/app/admin/controllers/report.js
@@ -2,9 +2,9 @@ angular.module('proton.admin')
 .controller('ReportController', function(payments) {
     var vm = this;
 
-    vm.GetPayments = (CreateTime) => {
+    vm.GetSubscriptions = (CreateTime) => {
         var time = (CreateTime === undefined) ? new Date().getTime() : CreateTime;
-        payments.get(time)
+        payments.GetSubscriptions(time)
         .then(({data}) => {
             vm.Subscriptions = data;
         });
