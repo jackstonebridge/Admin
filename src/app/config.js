bd4e29b83bce4ef647aaff04b322209600b5c04d
Martin Zeman <martin.zeman@protonmail.com>
2017-01-25 17:32:34 +0100

    Fix config

--- src/app/config.js
+++ src/app/config.js
@@ -3,14 +3,13 @@ angular.module('proton.config', [])
     version: '0.2.2',
 
     // Production settings
-    // debug: false,
-    // url: 'https://protonmail.com',
-    // apiUrl: 'https://admin-api.protontech.ch',
-    // app_version: '0.1.0',
-    // api_version: '1',
-    // date_version: 'Fri Dec 23 2016',
-    // clientID: 'Admin',
-    // clientSecret: 'c916d8e8712f96c719acab4ec54e7844'
+    debug: false,
+    url: 'https://protonmail.com',
+    apiUrl: 'https://admin-api.protontech.ch',
+    app_version: '0.1.0',
+    api_version: '1',
+    clientID: 'Admin',
+    clientSecret: 'c916d8e8712f96c719acab4ec54e7844'
 
     // Test settings
     // debug: true,
@@ -18,21 +17,19 @@ angular.module('proton.config', [])
     // apiUrl: 'https://test-api.protonmail.ch',
     // app_version: '0.1.0',
     // api_version: '1',
-    // date_version: 'Fri Dec 23 2016',
     // clientID: 'Admin',
     // clientSecret: 'c916d8e8712f96c719acab4ec54e7844'
 
     // Local enviroment settings
-    debug: true,
+    // debug: true,
     // url: 'https://localhost',
-    url: 'https://protonmail.dev',
+    // url: 'https://protonmail.dev',
     // url: 'https://protonmail.host',
     // apiUrl: 'https://localhost/api',
-    apiUrl: 'https://protonmail.dev/api',
+    // apiUrl: 'https://protonmail.dev/api',
     // apiUrl: 'https://protonmail.host/api',
-    app_version: '0.1.0',
-    api_version: '1',
-    date_version: 'Fri Dec 23 2016',
-    clientID: 'Admin',
-    clientSecret: 'c916d8e8712f96c719acab4ec54e7844'
+    // app_version: '0.1.0',
+    // api_version: '1',
+    // clientID: 'Admin',
+    // clientSecret: 'c916d8e8712f96c719acab4ec54e7844'
 });
